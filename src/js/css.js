98998e56fcfb7665c4d0c62586d6cc2b737b7644
Christian Wang <christian.gnaw@gmail.com>
2017-11-07 21:40:44 -0500

    Completed basic virtual dom implementation

--- src/js/css.js
+++ src/js/css.js
@@ -1,22 +1,22 @@
 //Figure our what selector is
-function checkSelector(element, selector) {
+function checkSelector(selector) {
   let selected = {
     "classList": [],
     "id": []
   };
 
   //Eventually remove this with regex
-  selected["classList"] = selector.match(/\.\w+/g);
-  for (let i = 0; i < selected["classList"].length; i++) {
-    selected["classList"][i] = selected["classList"][i].replace(".", "");
+  selected.classList = selector.match(/\.\w+/g);
+  for (let i = 0; i < selected.classList.length; i++) {
+    selected.classList[i] = selected.classList[i].replace(".", "");
   }
 
-  selected["id"] = selector.match(/\#\w+/g);
-    for (let i = 0; i < selected["id"].length; i++) {
-      selected["id"][i] = selected["id"][i].replace("#", "");
+  selected.id = selector.match(/\#\w+/g);
+    for (let i = 0; i < selected.id.length; i++) {
+      selected.id[i] = selected.id[i].replace("#", "");
     }
 
   return selected;
 }
 
-console.log(checkSelector(null, ".class .intro#ok"));
+console.log(checkSelector( ".class .intro#ok"));
--- src/js/silk.js
+++ src/js/silk.js
@@ -1,11 +1,27 @@
 class SilkElement {
-  constructor(element, properties, value) {
-    this.element = element;
-    this.properties = properties;
-    this.value = value;
+  constructor(tagName, options, children) {
+    this.tagName = tagName;
+    this.options = options;
 
     //Array full of children elements
-    this.children = [];
+    this.children = (children) ? children : [];
+  }
+
+  convertToHTML() {
+    let html = "";
+    if (this.tagName)
+      html += "<" + this.tagName + ">";
+    if (Array.isArray(this.children)) {
+      this.children.forEach(function(child) {
+        html += child.convertToHTML();
+      });
+    } else {
+      html += this.children;
+    }
+    if (this.tagName)
+      html += "</" + this.tagName + ">";
+
+      return html;
   }
 
   appendChild(childElement) {
@@ -16,21 +32,67 @@ class SilkElement {
     this.children.splice(index, 0, childElement);
   }
 
-  querySelector(selector) {
-    let selected = null;
-
+  removeChild(childElement) {
+    let index = this.children.indexOf(childElement);
+    if (index > -1) {
+      this.children.splice(index, 1);
+      return;
+    }
     this.children.forEach(function(child) {
-      child.properties[selector]
+      let index = child.children.indexOf(childElement);
+      if (index > -1) {
+        child.children.splice(index, 1);
+        return;
+      }
+      return child.removeChild(childElement);
     });
+  }
 
+  querySelector(selector) {
+    return querySelector(selector)[0];
+  }
+
+  querySelectors(selector) {
+    let selected = [];
+    this.children.forEach(function(child) {
+      if (child.options[selector]) {
+        selected.push(child);
+      } else {
+        child.querySelector(selector).forEach(function(e) {
+          selected.push(e);
+        });
+      }
+    });
     return selected;
+  }
+
+  getElementsByTagName(tagName) {
+    let elements = [];
+    this.children.forEach(function(child) {
+      if (child.tagName === tagName) {
+        elements.push(child);
+      } else {
+        child.getElementsByTagName(tagName).forEach(function(e) {
+          elements.push(e);
+        });
+      }
+    });
+    return elements;
   }
 }
 
 
 class Silk {
+  constructor() {
+    this.VOM = [];
+  }
 
   createElement(tagName, options) {
-
+    let createdElement = new SilkElement(tagName, options);
+    this.VOM.push(createdElement);
+    return createdElement;
   }
 }
+
+let doc = new SilkElement("html", {}, [new SilkElement("head", {}, [new SilkElement("title", {}, "Hey Jude!")]), new SilkElement("body", {})]);
+console.log(doc.convertToHTML());
